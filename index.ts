212414d14dcf07cdaa1c5f41207717070a4ef381
NPGaming2971 <haht245@gmail.com>
2024-09-05 20:17:08 +0700

    function to login to google

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { connect } from "puppeteer-real-browser";
+import { connect, type PageWithCursor } from "puppeteer-real-browser";
 import PuppeteerStealthPlugin from "puppeteer-extra-plugin-stealth";
 import { getIsolatedBrowserPath } from "./utils.js";
 import { ChromeFlags } from "./constants.js";
@@ -19,6 +19,29 @@ async function main() {
 	});
 
 	await page.goto("https://shell.cloud.google.com", { waitUntil: "domcontentloaded" });
+}
+
+async function loginToGoogle(page: PageWithCursor, email: string, password: string) {
+	const EmailInputSelector = 'input[type="email"]';
+	const EmailNextSelector = "#identifierNext";
+	const PasswordInputSelector = `input[type="password"]`;
+	const PasswordNextSelector = "#passwordNext";
+
+	await page.waitForSelector(EmailInputSelector);
+	await page.realClick(EmailInputSelector);
+	await page.locator(EmailInputSelector).fill(email);
+
+	await page.waitForSelector(EmailNextSelector);
+	await page.realClick(EmailNextSelector);
+
+	await page.waitForSelector(PasswordInputSelector);
+	await page.realClick(PasswordInputSelector);
+	await page.locator(PasswordInputSelector).fill(password);
+
+	await page.waitForSelector(PasswordNextSelector);
+	await page.realClick(PasswordNextSelector);
+
+	await page.waitForNavigation();
 }
 
 main();
