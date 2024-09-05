8fe9bcea473316bba5726c151126ae9bb4edb5b6
NPGaming2971 <haht245@gmail.com>
2024-09-05 20:15:57 +0700

    use self downloaded chrome instead

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
 import { connect } from "puppeteer-real-browser";
 import PuppeteerStealthPlugin from "puppeteer-extra-plugin-stealth";
+import { getIsolatedBrowserPath } from "./utils.js";
 import { ChromeFlags } from "./constants.js";
 
 async function main() {
+	const browserPath = await getIsolatedBrowserPath();
 	const { page } = await connect({
 		headless: false,
 		args: ChromeFlags,
 		turnstile: false,
+		customConfig: {
+			chromePath: browserPath,
+		},
 		connectOption: {
 			ignoreHTTPSErrors: true,
 		},
 		plugins: [PuppeteerStealthPlugin()],
 	});
 
-	await page.goto("https://shell.cloud.google.com", { waitUntil: 'domcontentloaded' });
-
+	await page.goto("https://shell.cloud.google.com", { waitUntil: "domcontentloaded" });
 }
 
 main();
